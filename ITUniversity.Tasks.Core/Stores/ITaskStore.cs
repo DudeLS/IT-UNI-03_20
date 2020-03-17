ca11750775746240d1e11281096a03fda286d4ca
s.lyalin <s.lyalin@mail.elewise.com>
2020-03-17 20:08:17 +0300

    main

---
+++ ITUniversity.Tasks.Core/Stores/ITaskStore.cs
@@ -0,0 +1,34 @@
+﻿using ITUniversity.Tasks.Entities;
+
+namespace ITUniversity.Tasks.Stores
+{
+    /// <summary>
+    /// Хранилище для сущности <see cref="TaskBase"/>
+    /// </summary>
+    public interface ITaskStore
+    {
+        /// <summary>
+        /// Сохранить
+        /// </summary>
+        /// <param name="task">Задача</param>
+        TaskBase Save(TaskBase task);
+
+        /// <summary>
+        /// Обновить
+        /// </summary>
+        /// <param name="task">Задача</param>
+        TaskBase Update(TaskBase task);
+
+        /// <summary>
+        /// Получить
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        TaskBase Get(long id);
+
+        /// <summary>
+        /// Удалить
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        void Delete(long id);
+    }
+}
---
+++ ITUniversity.Tasks.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+using ITUniversity.Tasks.Entities;
+using ITUniversity.Tasks.Stores;
+using ITUniversity.Tasks.Web.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ITUniversity.Tasks.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        private readonly ITaskStore taskStore;
+
+        public HomeController(ILogger<HomeController> logger, ITaskStore taskStore)
+        {
+            this.logger = logger;
+            this.taskStore = taskStore;
+        }
+
+        public IActionResult Index()
+        {
+            var a = taskStore.Save(new TaskBase { Subject = "Hello", Description = "Hello world!!!" });
+
+            a.Subject = "New";
+            var d = taskStore.Update(a);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
